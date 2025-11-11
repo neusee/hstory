fef970678429f602cd93fe7c4ce4115a5e3b4177
윤효진 <hyojinee@dgsw.hs.kr>
2025-11-11 23:23:11 +0900

    feat: 레이아웃 정의

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,10 +1,4 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
-
 export default function Header() {
-    const router = useRouter();
-
     return (
         <div>헤더</div>
     );
