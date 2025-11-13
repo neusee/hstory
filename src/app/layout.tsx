5aca866c9b781e89d306d2d0e77ce28563ade5aa
윤효진 <hyojinee@dgsw.hs.kr>
2025-11-13 18:04:18 +0900

    chore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,19 @@
 import { Metadata } from "next";
-import localFont from "next/font/local";
-import Header from "@/components/common/header";
-import Footer from "@/components/common/footer";
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
     title: "hstory",
-    description: "프론트엔드 윤효진의 개발 블로그",
+    description: "프론트엔드 개발자 윤효진의 기술 블로그",
 };
 
-const pretendard = localFont({
-    src: "../../public/fonts/PretendardVariable.woff2",
-    display: "swap",
-});
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode,
+}) {
     return (
-        <html lang="ko" className={ pretendard.className }>
-            <body className="">
-                <Header />
-                <main className="flex flex-col">
-                    { children }
-                </main>
-                <Footer />
-            </body>
+        <html lang="ko">
+            <body>{children}</body>
         </html>
     );
 }
